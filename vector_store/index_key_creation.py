6b022985c0e0697d10338912a004bd8ffad1d4f3
Srivatsa Kundurthy <kundurthy.srivatsa@gmail.com>
2025-02-08 15:18:01 -0500

    update code for chunk embedding

--- vector_store/index_key_creation.py
+++ vector_store/index_key_creation.py
@@ -13,16 +13,22 @@ JSON_FILE = "../database_population/doc_inclusion.json"
 # Read JSON file and extract keys (keywords) and values (chunks)
 keys = []
 chunks = []
+#concats = []
 with open(JSON_FILE, "r", encoding="utf-8") as f:
     data = json.load(f)  # Load JSON as a dictionary
 
     for key, chunk in data.items():
+        #print(key)
         keys.append(key)  # Store the comma-separated keyword string
         chunks.append(chunk)  # Store the corresponding text chunk
 
+
+#print(len(chunks))
+#print(len(keys))
+concats = [keys[i] + chunks[i] for i in range(len(chunks))]
 # Generate embeddings for the keys
 print("Generating embeddings for keywords...")
-embeddings = model.encode(keys, convert_to_numpy=True, normalize_embeddings=True)
+embeddings = model.encode(concats, convert_to_numpy=True, normalize_embeddings=True)
 
 # Create a FAISS index
 embedding_dim = embeddings.shape[1]
@@ -32,7 +38,7 @@ index = faiss.IndexFlatL2(embedding_dim)  # L2 (Euclidean distance)
 index.add(embeddings)
 
 # Save the FAISS index
-faiss.write_index(index, "faiss_roman_keywords.index")
+faiss.write_index(index, "faiss_roman_concats.index")
 
 # Save metadata mapping (Index -> Key -> Chunk)
 metadata_file = "metadata.json"
